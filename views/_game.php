207051d3c549042fe4fa31c671e78ad658a91def
Rajesh Kumar <rswaminathan@zynga.com>
2012-12-27 17:35:19 -0500

    Only show submit button when it's my turn

--- views/_game.php
+++ views/_game.php
@@ -8,32 +8,32 @@ $scores = $game->get_scores();
 ?>
 
 <form method="post" action="<?=$this->_url($game->form_action())?>">
-    <?=csrf::html_tag()?>
+	<?=csrf::html_tag()?>
 
-    <? if (empty($other_player->id)): ?>
-        <p>Start a new game with (type friend&#39;s email):
-        <input type="text" name="player2_email" size="30" /></p>
-    <? elseif ($game->is_game_over()): ?>
-        <p>
-            <strong>Game over!</strong>
-            <? if ($game->did_i_win()): ?>
-                You won!
-            <? else: ?>
+	<? if (empty($other_player->id)): ?>
+		<p>Start a new game with (type friend&#39;s email):
+		<input type="text" name="player2_email" size="30" /></p>
+	<? elseif ($game->is_game_over()): ?>
+		<p>
+			<strong>Game over!</strong>
+			<? if ($game->did_i_win()): ?>
+				You won!
+			<? else: ?>
 				You lost :(
-            <? endif; ?>
-        </p>
-    <? elseif ($myturn): ?>
-        <p><strong>Your move with <?=hsc($other_player->email)?>!</strong></p>
+			<? endif; ?>
+		</p>
+	<? elseif ($myturn): ?>
+		<p><strong>Your move with <?=hsc($other_player->email)?>!</strong></p>
 		<? if (!empty($last_word_played)): ?>
 			<p>They played: <em><?=hsc($last_word_played)?></em></p>
 		<? endif; ?>
-    <? else: ?>
-        <p>Waiting for move from <?=hsc($other_player->email)?></p>
+	<? else: ?>
+		<p>Waiting for move from <?=hsc($other_player->email)?></p>
 		<? if (!empty($last_word_played)): ?>
 			<p>You played: <em><?=hsc($last_word_played)?></em></p>
 		<? endif; ?>
 	<? endif; ?>
-    
+
 	<p name="score">Score:
 		<? echo spf(($scores['winning'] == 'you') ? '<strong>You (%s)</strong>' : 'You (%s)', $scores['you']) ?>,
 		<? echo spf(($scores['winning'] == 'them') ? '<strong>Them (%s)</strong>' : 'Them (%s)', $scores['them']) ?>
@@ -44,14 +44,14 @@ $scores = $game->get_scores();
 		<p class="letters">Your word: <span id="word_<?=$game->id?>"></span></p>
 	<? endif; ?>
 
-    <div class="game_div <?=$game_active ? 'enabled' : 'disabled'?>" game-id="<?=$game->id?>" my-score="<?=hsc($scores['you'])?>" their-score="<?=hsc($scores['them'])?>">
-        <table>
-            <tr>
-                <?php
-                foreach ($game->letters as $i => $letter) {
-                	if ($i != 0 && $i % 5 == 0) {
-                		echo '</tr><tr>';
-                	}
+	<div class="game_div <?=$game_active ? 'enabled' : 'disabled'?>" game-id="<?=$game->id?>" my-score="<?=hsc($scores['you'])?>" their-score="<?=hsc($scores['them'])?>">
+		<table>
+			<tr>
+				<?php
+				foreach ($game->letters as $i => $letter) {
+					if ($i != 0 && $i % 5 == 0) {
+						echo '</tr><tr>';
+					}
 					// figure out the state of this tile
 					$class_map = array(0 => '', 1 => 'cplayer', -1 => 'oplayer');
 					$class = $class_map[$game->get_tile_state($i)];
@@ -59,15 +59,17 @@ $scores = $game->get_scores();
 					// figure out the deltas of each person's score when this tile is played
 					$deltas = $game->get_tile_deltas($i);
 					echo spf('<td class="%s" data-coord="%d" delta-me="%d" delta-them="%d">%s</td>', $class, hsc($i), $deltas['me'], $deltas['them'], hsc($letter));
-                }
-                ?>
-            </tr>
-        </table>
-    </div>
+				}
+				?>
+			</tr>
+		</table>
+	</div>
 
-    <p><input type="submit" name="btn_submit" value="Submit Move!" /></p>
+	<? if ($myturn): ?>
+		<p><input type="submit" name="btn_submit" value="Submit Move!" /></p>
+	<? endif; ?>
 
-    <? if (!empty($other_player->id)): ?>
-        <p><a class="warning" href="<?=spf('%s?%s', $this->_url('/game/delete/' . $game->id), csrf::param()) ?>">Delete Game</a></p>
+	<? if (!empty($other_player->id)): ?>
+		<p><a class="warning" href="<?=spf('%s?%s', $this->_url('/game/delete/' . $game->id), csrf::param()) ?>">Delete Game</a></p>
 	<? endif; ?>
 </form>
